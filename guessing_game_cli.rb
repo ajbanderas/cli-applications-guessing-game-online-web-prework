4c453f0d6e9b3b6de08684371b68ec36adf69a46
Kevin Downs <kevin.downs04@gmail.com>
2019-11-01 05:50:27 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,9 +4,23 @@ def generate_number
 end
 
 def prompt_user
-  
+  "Hello, guess a number between 1 and 6."
+end
+
+def get_user_input
+  gets
+end
+
+def compare_numbers(computer_num, player_num)
+  computer_num == player_num
+end
+
+def display_result(result)
+  result ? 
 end
 
 def run_guessing_game
-
+  computer_num = generate_number
+  prompt_user
+  player_num = get_user_input
 end

dac28cb305b8dfc89b84ad6db0483b20d0a05d82
Kevin Downs <kevin.downs04@gmail.com>
2019-11-01 06:16:12 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -12,13 +12,20 @@ def get_user_input
 end
 
 def end_game(computer_num, player_input)
-  
+  if player_input == "exit"
+    puts "Goodbye!"
+  elsif computer_num == player_input
+    puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{computer_num}."
+  end
 end
 
 def run_guessing_game
   computer_num = generate_number
   prompt_user
   player_input = get_user_input
-  end_game(computer_num, player_input)
+  pp computer_num
+  pp player_input
 end
 
